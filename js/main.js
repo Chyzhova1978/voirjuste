2007f20dbcc463057b45bfd3c865cd41a2c0ad18
Chyzhova1978 <cizovazanna510@gmail.com>
2025-06-14 18:01:56 +0300

    first update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// try {
+//   window.addEventListener("DOMContentLoaded", () => {
+//     const burgerIcon = document.querySelector(".header__burger-menu");
+//     const burgerMenu = document.querySelector(".menu");
+//     const closeButtonMenu = document.querySelector(".menu__close");
+
+//     burgerIcon.addEventListener("click", () => {
+//       burgerMenu.style = `display: block`;
+//     });
+
+//     closeButtonMenu.addEventListener("click", () => {
+//       burgerMenu.style = `display: none`;
+//     });
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
